63f4957b473c2b28b6b1a90af723308e3257cbf8
bakukousei <627229645@qq.com>
2020-08-13 12:44:49 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 import './plugins/element.js'
 import TreeTable from 'vue-table-with-tree-grid'
 
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+//导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 //全局样式表
 import './assets/global.css'
 import axios from 'axios'
@@ -16,6 +23,23 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.component('tree-table', TreeTable)
+//将富文本编辑器，注册为全局可用的变量
+Vue.use(VueQuillEditor)
+
+
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
